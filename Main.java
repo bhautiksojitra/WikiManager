b4863cb70bc4c98754796586324655caa4531483
bhautiksojitra <48924148+bhautiksojitra@users.noreply.github.com>
2021-02-15 13:42:39 -0600

    changes in Main & InputProcess
    closed the files after using Scanner in Main and File Reader in InputProcess.java

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+
 /*----------------------------------------------------
 
     Class: Main
@@ -5,17 +6,19 @@
     Purpose: Get Input from the User and Call the file processor.
 
  ------------------------------------------------------*/
- import java.util.*;
- public class Main {
+import java.util.*;
 
-    public static void main(String[] args)
-    {
+public class Main {
+
+    public static void main(String[] args) {
         System.out.println("Enter File Name with extention: ");
-        
+
         Scanner scanFileName = new Scanner(System.in);
         String fileName = scanFileName.next();
-        
-        //Initialising the file reader
+
+        // Initialising the file reader
         InputProcess i = new InputProcess(fileName);
+
+        scanFileName.close();
     }
 }
